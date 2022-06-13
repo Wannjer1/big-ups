490da36dc89898521f87d2106a999c03d4d26193
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-13 19:29:05 +0300

    added path to the home page

---
+++ bigup_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include 
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from django.contrib.auth import views as auth_view
+
+
+urlpatterns = [
+    path('',views.home, name='home'),
+
+]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
