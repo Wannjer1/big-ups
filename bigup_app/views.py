4d4072a4082a305d79548629f9e0cafde7e4da54
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-13 19:29:36 +0300

    added static and templates folders

--- bigup_app/views.py
+++ bigup_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
 
 # Create your views here.
+#home function that displays all posts
+def home(request):
+
+    return render(request, 'bigup/index.html')
