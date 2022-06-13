3563735927a29dcf846b2987fc11ed46d95436be
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-13 20:10:03 +0300

    extended base and added navbar

--- bigup_app/models.py
+++ bigup_app/models.py
@@ -1,6 +1,9 @@
-from email.policy import default
+
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from requests import delete
 
 # Create your models here.
 class Profile(models.Model):
@@ -21,3 +24,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+class Project(models.Model):
+    webimage = models.ImageField(upload_to='webimage/',null=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    link = models.URLField()
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+        
