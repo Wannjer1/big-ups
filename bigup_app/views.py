9c5dbf6060b375c95811671d760e308b7a18a403
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-15 12:33:33 +0300

    created logout view function

--- bigup_app/views.py
+++ bigup_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render,redirect
+from django.urls import reverse
 from .models import Profile,Project
 from django.http import HttpResponse,Http404
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import *
+from django.contrib import messages
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -18,19 +22,48 @@ def home(request):
     return render(request, 'bigup/index.html')
 
 # signup view function
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        user = User.objects.create_user(username=username, email=email,password=password1)
-        user.save()
-        profile=Profile.objects.create(user=user,email=user.email)
+def signup_user(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+         
+            form.save()
+            messages.success(request, "Registration successful." )
+            return redirect(reverse("login"))
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = RegisterForm()
+    context={"form":form}
+    return render (request, "registration/register.html", context)
 
-        return redirect('login')
+
+# login view function
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('home')
+            # else:
+            #     messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+
     else:
-        return render(request,'registration/register.html')
+        form = AuthenticationForm()
+    return render (request, 'registration/login.html', context={"login_form": form})
+
+def logout(request):
+    logout(request)
+
+    return render (request, 'registration/logout.html')
+
+
 
 # APIView section
 def bigapi(request):
