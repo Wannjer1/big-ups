02649e3953c86514079bc8b308d42bb45a90b479
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-15 14:32:35 +0300

    overrided the ready() method of the bigup_app
     to perform initialization task which is registering signals.

--- bigup_app/apps.py
+++ bigup_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BigupAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bigup_app'
+
+    def ready(self):
+        import bigup_app.signals
