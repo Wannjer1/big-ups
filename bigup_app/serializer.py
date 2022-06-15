aa436cf0a0950dcdfd2945502dafa9e66150e5f6
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-15 06:39:58 +0300

    bg-bug fix

--- bigup_app/serializer.py
+++ bigup_app/serializer.py
@@ -2,7 +2,7 @@
 
 
 from rest_framework import serializers
-from .models import Profile,Project,Rating
+from .models import Profile,Project
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
