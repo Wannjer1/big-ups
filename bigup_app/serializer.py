48f409e8a188797e856af5d5cecd5a088e02c3ba
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-15 06:08:23 +0300

    added projectserializer and profile serializer class

---
+++ bigup_app/serializer.py
@@ -0,0 +1,15 @@
+
+
+
+from rest_framework import serializers
+from .models import Profile,Project,Rating
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Profile
+        fields=('bio','email','profile_pic','user')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Project
+        fields=('webimage','name','description','link','profile','no_of_ratings')
