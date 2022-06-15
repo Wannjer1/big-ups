0db65e0347b0b24a3780e7c5427b5716a532d339
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-15 14:36:53 +0300

    created update profile form

---
+++ bigup_app/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Profile, Project
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+
+
+# signup form
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+# update profile form
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
+    
