4887b7802e3e9624b01d60a6a0b9e80e55a50351
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-15 21:05:51 +0300

    added media file os

--- bigup_project/settings.py
+++ bigup_project/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'bigup_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
