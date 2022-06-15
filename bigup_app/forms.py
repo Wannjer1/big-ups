7315172f7d18e347963ec38eba8c09799a7e62af
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-15 20:56:04 +0300

    altered profile form meta class

--- bigup_app/forms.py
+++ bigup_app/forms.py
@@ -1,3 +1,4 @@
+import email
 from django import forms
 from .models import Profile, Project
 from django.contrib.auth.models import User
@@ -17,8 +18,13 @@ class RegisterForm(UserCreationForm):
 class UpdateProfileForm(forms.ModelForm):
     avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
     bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    name = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    email = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
 
     class Meta:
+    # class Meta enables us to to create a form based on a model
         model = Profile
-        fields = ['avatar', 'bio']
+        fields = ['avatar', 'bio','name', 'email']
+
+
     
